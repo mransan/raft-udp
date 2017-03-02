a4183185c06686404c65e773f09bc8b69d59f2cf
mransan <maxime.ransan@gmail.com>
2017-03-02 11:03:29 -0500

    Add start_server.ml

---
+++ tests/start_server.ml
@@ -0,0 +1,73 @@
+
+module Conf = Raft_com_conf
+
+let () = 
+  let id   = ref (-1) in
+  let id_spec = Arg.Set_int id in
+  
+  let log = ref false in 
+  let log_spec = Arg.Set log  in
+
+  let env, env_spec = Conf.env_arg in
+  
+  let task = ref "" in 
+
+  Arg.parse [
+    ("--env", env_spec , " : which env");
+    ("--id" , id_spec  , " : server raft id");
+    ("--log", log_spec , " : enable logging");
+  ] (function
+    | "counter" -> task := "counter_srv.native" 
+    | "hash" -> task := "hash_srv.native" 
+    | _ -> print_endline "Invalid app name"; exit 1
+  ) "start_server [options] <app name>";
+  
+  if !task = ""
+  then begin 
+    print_endline "Missing app name"; 
+    exit 1
+  end;
+
+  let {
+    Conf.disk_backup = {Conf.log_record_directory};
+    _ 
+  } = Conf.default_configuration !env in 
+
+  begin 
+    ignore @@ 
+      Sys.command @@ Printf.sprintf "rm -rf %s/*.data" log_record_directory; 
+    ignore @@ 
+      Sys.command @@ "rm -f *.log"
+  end; 
+
+  (* Launching App server *)
+
+  begin 
+    let args = !task :: [] in 
+    let args = 
+      if !log
+      then args @ ("--log")::[]
+      else args
+    in 
+    let args = args @ ("--id")::(string_of_int !id)::[] in
+    let args = args @ ("--env")::(Conf.string_of_env !env) :: [] in 
+
+    match Unix.fork () with
+    | 0 -> Unix.execv !task (Array.of_list args)
+    | _ -> ()
+  end;
+  
+  (* Launching RAFT server *)
+
+  begin 
+
+    let args = "server.native" :: [] in 
+    let args = 
+      if !log
+      then args @ ("--log")::[]
+      else args
+    in 
+    let args = args @ ("--id")::(string_of_int !id)::[] in
+    let args = args @ ("--env")::(Conf.string_of_env !env) :: [] in 
+    Unix.execv "server.native" (Array.of_list args)
+  end
